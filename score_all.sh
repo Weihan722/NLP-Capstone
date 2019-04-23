eb064a32656dd5fe3d4c8c8bc0f3651a1eee4580
yuchong xiang <Yvonna@D-10-18-180-87.dhcp4.washington.edu>
2019-04-23 11:45:23 -0700

    modify scoring file to generate summary scores

--- score_all.sh
+++ score_all.sh
@@ -1,6 +1,13 @@
 WORDS="coach education execution figure job letter match mission mood paper post pot range rest ring scene side soil strain test"
+OUTFILE="summary_results.txt"
 
+rm -rf $OUTFILE
 for WORD in $WORDS; do
-	echo perl scorer.pl baseline_results/"$WORD".txt wsd2-master/data/test/fr/"$WORD"_gold.txt -t best
-	perl scorer.pl baseline_results/"$WORD".txt wsd2-master/data/test/fr/"$WORD"_gold.txt -t best
+	echo perl scorer.pl baseline_results/"$WORD".txt fr/"$WORD"_gold.txt -t best
+	perl scorer.pl baseline_results/"$WORD".txt fr/"$WORD"_gold.txt -t best
+	precision=`sed '3q;d' baseline_results/"$WORD".txt.results | awk -F',' ' {print $1} ' | awk '{print $3}'`
+	echo $WORD $precision >> $OUTFILE
 done
+
+avg=`awk '{ total += $2; count++ } END { print total/count }' $OUTFILE`
+echo 'AVG' $avg >> $OUTFILE
